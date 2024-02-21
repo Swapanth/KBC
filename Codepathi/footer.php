d7c7aa189a95bf8551676210e005aa3608125c63
Swapanth <swapanthvakapalli@gmail.com>
2024-02-21 21:51:40 +0530

    swapanth

---
+++ Codepathi/footer.php
@@ -0,0 +1,30 @@
+     <footer class="main-footer" style='background-color:rebeccapurple; padding:2px;border-top:1px solid #666;'>
+<!--        <div class="pull-right hidden-xs hidden-sd">
+          <b>Version</b> 2.0
+        </div>
+-->		
+<!--      <div align='center' id='fnews'>
+		&copy; 2020 <b>Bhimavaram Online Housie<br>Developed by <a href='http://www.mcr.org.in' target='_new'>MCR Web Solutions</a></b> Bhimavaram
+        <a href='http://www.mcr.org.in' target='_new'><img src='dist/img/mcr.png'></a>
+		</div>      
+		-->
+		<div id='myCarousel' class='carousel slide' data-ride='carousel'>
+		  <div class='carousel-inner' id='fnews' style='text-align:center;'>
+            <div class='item-active'><b>Developed by <a href='http://www.mcr.org.in' target='_new' style="color: whitesmoke;">MCR Web Solutions</a></b><br>SRKREC, Bhimavaram | &copy; 2020</div>
+          </div>
+		 </div>
+<body>
+  <style>
+    .carousel-inner{
+      background-color: rebeccapurple;
+    }
+    .item-active{
+      color: whitesmoke;
+    }
+    
+  </style>
+</body>
+<!-- &copy; 2020 <b>Bhimavaram Online Housie<br>Developed by <a href='http://www.mcr.org.in' target='_new'>MCR Web Solutions</a></b> Bhimavaram
+        <a href='http://www.mcr.org.in' target='_new'><img src='dist/img/mcr.png'></a>      
+-->		
+		</footer>
